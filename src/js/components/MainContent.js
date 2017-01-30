91a7cfa72c8b979fb79c84f20d202bef496146ad
Alex Whitlock <alex.whitlock@uk.tesco.com>
2017-01-30 21:09:44 +0000

    Setup for lat and long

--- src/js/components/MainContent.js
+++ src/js/components/MainContent.js
@@ -14,8 +14,8 @@ const ErrorMessage = (props) => {
 
   return (
     <div>
-    <div>{theErrorMessage}</div>
-    <div>{theLoadingMessage}</div>
+      <div>{theErrorMessage}</div>
+      <div>{theLoadingMessage}</div>
     </div>
   );
 };
@@ -25,68 +25,53 @@ class MainContent extends React.Component {
 
   constructor(props) {
     super(props);
+    this._onSubmit = this._onSubmit.bind(this);
+    this._getLocation = this._getLocation.bind(this);
     this.state = {
+      location_lat: null,
+      location_long: null,
       venueData: null,
       errorMessage: null,
       loading: true
     };
   }
 
-  componentWillMount() {
-    const obj = this;
+  _getLocation(location) {
+    this.setState({ location_lat: location.coords.latitude });
+    this.setState({ location_long: location.coords.longitude });
+  }
 
-    // Ajax into the data, set as const
-    const request = new XMLHttpRequest();
-    request.open('GET', obj.props.apiSource, true);
-    request.onload = function () {
-      if (this.status >= 200 && this.status < 400) {
-        // Success!
-        obj.setState({
-          venueData: this.response,
-          loading: false
-        });
-      } else {
-        // We reached our target server, but it returned an error
-        obj.setState({
-          errorMessage: "Error getting data"
-        });
-      }
-    };
-    request.onerror = function () {
-      obj.setState({
-        errorMessage: "Error connecting to Server"
-      });
-    };
-    request.send();
+  _onSubmit(e) {
+    e.preventDefault();
+
+    if (!this.state.location_lat) {
+      navigator.geolocation.getCurrentPosition(this._getLocation);
+    } else {
+      
+      // Now we have the lat and long, get the venue info
+
+    }
   }
 
   render() {
-    let nearestVenue, theErrorMessage, LoadingMessage;
+    let theErrorMessage, LoadingMessage;
 
     // If there is data, display all the different parts.
     if (this.state.venueData) {
       const theVenueJson = this.state.venueData;
       const theVenueParse = JSON.parse(theVenueJson);
-
-      // Show Cheapest venue, for each flight
-      nearestVenue = theVenueJson.map(function (element, index) {
-        return (
-          <EachVenueOption theData={element} />
-        );
-      });
     }
 
     return (
       <div className="venue-view clearfix">
         
-        <VenueSearch />
+      <form action="#" method="GET" id="searchform" onSubmit={this._onSubmit}>
+        <input type="text" name="query" id="query" value="Example: Coffee" />
+        <input type="submit" name="submit" id="submit" value="Submit" />
+      </form>
+        
         <ErrorMessage error={this.state.errorMessage} loading={this.state.loading} />
   
-        <div className="nearest--venue clearfix">
-          <h2><span>Nearest Venues</span></h2>
-          <div>{nearestVenue}</div>
-          <ErrorMessage loading={this.state.loading} />
-        </div>
       </div>
     );
   }
